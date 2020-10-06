2748e066625c351a93836617bbc565b9d88b6126
musaj <musajmahmoud@gmail.com>
2020-10-06 18:51:52 +0400

    Added test route

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+
+Route::get('/test', function() {
+    return "It works!";
+});
